35b97aee74d1fa90b8d114edf6af4db64190bbf5
shashankyadav03 <shashankyadav97@yahoo.in>
2020-06-06 01:20:20 +0530

    Learning JS

--- Test.js
+++ Test.js
@@ -10,3 +10,7 @@ var firstName = 'John';
 var age = 28;
 // Type coercion
 console.log(firstName + ' ' + age);
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
