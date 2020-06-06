cba8ed4b9f4abd51c5a5b8f4499e140c31ab9c8d
shashankyadav03 <shashankyadav97@yahoo.in>
2020-06-06 20:36:42 +0530

    updated index

--- Test.js
+++ Test.js
@@ -1,3 +1,4 @@
+/*
 var firstName = 'John';
 console.log(firstName);
 var lastName = 'Smith';
@@ -8,9 +9,16 @@ job = 'Teacher';
 console.log(job);
 var firstName = 'John';
 var age = 28;
+
 // Type coercion
+
 console.log(firstName + ' ' + age);
 var job, isMarried;
 job = 'teacher';
 isMarried = false;
 console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+//console.log(firstName)
+*/
+var firstName = 'John';
+var lastName=prompt("Last name plz");
+console.log(firstName+' '+lastName)
